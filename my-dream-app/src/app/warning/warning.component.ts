0d74a613face4208223f85706d5387a8c0527cf8
Skye <skye.simpso@gmail.com>
2017-12-11 19:00:38 +1300

    first commit

---
+++ my-dream-app/src/app/warning/warning.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-the-warning',
+    templateUrl: './warning.component.html',
+    styleUrls: ['./warning.component.css']
+})
+export class WarningComponent {
+    title = 'this is a warning, be aware';
+    warningId: number = 10;
+    warningStatus: string = 'low-warning'
+
+    getWarning() {
+        return this.warningStatus;
+    }
+
+    allowNewServer = false;
+    serverCreationStatus = 'No Server was created'
+
+    constructor() { 
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 2000);
+    }
+
+    ngOnInit() {
+
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'server was created';
+    }
+}
+
---
+++ my-dream-app/src/app/warning/warning.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { WarningComponent } from './warning.component';
+
+@NgModule({
+    declarations: [
+        WarningComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [WarningComponent]
+})
+export class WarningModule { }
