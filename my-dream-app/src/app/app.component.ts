c8db1d60ec21addfdcdbc972aa528bdda4f1a5ef
Skye <skye.simpso@gmail.com>
2017-12-17 11:57:31 +1300

    display details conditionally

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -9,6 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
   title = 'My Amazing App';
   username = '';
+  displayCondition = false;
+  elementArray = 0;
+  arrayNumbers = [];
+
+onDisplay() {
+  this.displayCondition = !this.displayCondition;
+  this.elementArray += 1;
+  this.arrayNumbers.push(this.elementArray);
+}
+
+getColor(p) {
+  return this.arrayNumbers.indexOf(p) >= 4 ? 'blue' : 'white';
+  // because first element in array is 0
+}
 }
 
 
